127288ee027b958c3b76a8e845e2a01c3d0af765
Jikardo Montez Wood <zeta2624@pop-os.home.local>
2026-07-02 14:09:44 -0400

    Update Controls

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ WIDTH = 1024
 HEIGHT = 768
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tibran Simulator")
+clock = pygame.time.Clock()
 
 SPACE_BLACK = (5, 5, 12)
 STAR_WHITE = (240, 240, 255)
 
-img1 = pygame.image.load("Visual Assets/galaxy.png")
-img2 = pygame.image.load("Visual Assets/galaxy2.png")
-img3 = pygame.image.load("Visual Assets/galaxy3.png")
+img1 = pygame.image.load("Visual Assets/galaxy.png").convert_alpha()
+img2 = pygame.image.load("Visual Assets/galaxy2.png").convert_alpha()
+img3 = pygame.image.load("Visual Assets/galaxy3.png").convert_alpha()
 
 bg_number = random.randint(1,6)
 bg_filename = f"Visual Assets/cosmic_background{bg_number}.jpg"
 
-bg_image = pygame.image.load(bg_filename)
+bg_image = pygame.image.load(bg_filename).convert()
 bg_surface = pygame.transform.scale(bg_image, (WIDTH, HEIGHT))
 
 galaxy_sprites = [
@@ -44,6 +45,7 @@ while len(star_list) < 100:
 
 
     if not too_close:
+
         chosen_sprite = random.choice(galaxy_sprites)
         star_list.append((x, y, chosen_sprite))
 
@@ -54,8 +56,20 @@ while True:
             sys.exit()
 
 
-    screen.fill(SPACE_BLACK)
 
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+
+
+                new_sprite = random.choice(galaxy_sprites)
+
+
+                star_list.append((mouse_x, mouse_y, new_sprite))
+
+
+    screen.fill(SPACE_BLACK)
+   
     screen.blit(bg_surface, (0, 0))
 
 
@@ -65,4 +79,10 @@ while True:
         sprite = star[2]
         screen.blit(sprite, (x_pos - 12, y_pos - 12))
 
+
     pygame.display.flip()
+
+
+   
+
+    clock.tick(60)
